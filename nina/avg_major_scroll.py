3d2daaea6768cfb583e6464f30931467bfe299e5
codewizz <c.chaiwat.ch@gmail.com>
2022-04-27 23:21:44 +0700

    updaet: complete for regular exam

--- nina/avg_major_scroll.py
+++ nina/avg_major_scroll.py
@@ -7,14 +7,28 @@ def avg_major(file_name):
         csv_reader = csv.reader(file, delimiter=",")
         header = next(csv_reader)[1:]
         prepare_data = []
+        for h in header:
+            prepare_data.append({h: []})
+
+        for row in csv_reader:
+            for p in range(len(prepare_data)):
+                prepare_data[p][header[p]].append(int(row[p + 1]))
+
         # TODO
         # Avg of subject
         # High score of subject
         # low score of subject
-        for row in csv_reader:
-            for i in range(len(header)):
-                print(header[i])
-                print(row[i + 1])
+        for i in ['avg', 'high', 'low']:
+            print(i, end="\n")
+            for d in prepare_data:
+                key = list(d.keys())[0]
+                if i == 'avg':
+                    output = sum(list(d.values())[0]) / len(list(d.values())[0])
+                elif i == 'high':
+                    output = max(list(d.values())[0])
+                else:
+                    output = min(list(d.values())[0])
+                print(f"  {key}: {output}")
 
 
 if __name__ == "__main__":
