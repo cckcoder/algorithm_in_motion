aaec58fbd9b31af3fd77d6bc6d2e65c45c825c5b
codewizz <c.chaiwat.ch@gmail.com>
2022-04-27 23:23:36 +0700

    code format

--- nina/avg_major_scroll.py
+++ nina/avg_major_scroll.py
@@ -1,9 +1,9 @@
-from hashlib import new
 import sys, csv
+
 
 def avg_major(file_name):
     CSV_PATH = f"./{file_name}"
-    with open(CSV_PATH, 'r', encoding='utf8') as file:
+    with open(CSV_PATH, "r", encoding="utf8") as file:
         csv_reader = csv.reader(file, delimiter=",")
         header = next(csv_reader)[1:]
         prepare_data = []
@@ -18,13 +18,13 @@ def avg_major(file_name):
         # Avg of subject
         # High score of subject
         # low score of subject
-        for i in ['avg', 'high', 'low']:
+        for i in ["avg", "high", "low"]:
             print(i, end="\n")
             for d in prepare_data:
                 key = list(d.keys())[0]
-                if i == 'avg':
+                if i == "avg":
                     output = sum(list(d.values())[0]) / len(list(d.values())[0])
-                elif i == 'high':
+                elif i == "high":
                     output = max(list(d.values())[0])
                 else:
                     output = min(list(d.values())[0])
